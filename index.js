88a740c407819a5681e0c599f6be4588b49b4f69
God Wandroid <cristowanderson@gmail.com>
2022-03-16 10:17:28 -0400

    ya no hay temporizador para el cumpleaños de yanna

--- index.js
+++ index.js
@@ -19,80 +19,66 @@ let x = true
 justOneTime( 10,  () => console.log('prueba de func'))
 document.addEventListener('DOMContentLoaded', () => {
 
-  const timeCounter = document.querySelector('#currentTime')
-  const timeContainer = document.querySelector('#timelack')
+  //const timeCounter = document.querySelector('#currentTime')
+  //const timeContainer = document.querySelector('#timelack')
 
-  const daySeconds = 86400
-  const hourSeconds = 3600
-  const minuteSeconds = 60
-
-
-  obtainTimeString = () => {
-    const time = new Date()
-    var hours = time.getHours()
-    let minutes = time.getMinutes()
-    var seconds = time.getSeconds()
+  //const daySeconds = 86400
+  //const hourSeconds = 3600
+  //const minuteSeconds = 60
 
 
-    let lackSeconds = daySeconds - ((hours * hourSeconds) + (minutes * minuteSeconds) + seconds)
-    let currentTimeString = `${hours} horas, ${minutes} minutos y ${seconds} segundos.`
+  //obtainTimeString = () => {
+    //const time = new Date()
+    //var hours = time.getHours()
+    //let minutes = time.getMinutes()
+    //var seconds = time.getSeconds()
+
+
+    //let lackSeconds = daySeconds - ((hours * hourSeconds) + (minutes * minuteSeconds) + seconds)
+    //let currentTimeString = `${hours} horas, ${minutes} minutos y ${seconds} segundos.`
 
 
 
-    const birthdayCongrat = document.querySelector('#birthdayCongrat')
+    //const birthdayCongrat = document.querySelector('#birthdayCongrat')
 
-    if (lackSeconds < 1 && lackSeconds > -60) {
-      birthdayCongrat.innerHTML = "!!!Feliz cumpleaños, querida prima¡¡¡ Deseo que tu vida esté repleta de felicidad y cosas buenas y que puedas lograr tus metas y deseos. Espero que hayan muchísimos más días como este y que cada uno sea mejor que el anterior. . "
+    //if (lackSeconds < 1 && lackSeconds > -60) {
 
-      birthdayCongrat.style.textAlign = 'left'
-      document.querySelector('h1').innerHTML = 'Feliz 23 años, yuca.'
-
-      if (x === true) {
-        const yannaImage = document.createElement('img')
-        yannaImage.src = 'yanna_picture.jpeg'
-
-
-        document.querySelector('body').append(yannaImage)
-        yannaImage.style.width = '60vw'
-
-        const audio = document.createElement('audio')
-        audio.src = 'happy_birthday.mp3'
-        audio.autoplay = true
-
-        document.querySelector('body').appendChild(audio)
-
-      }
-
-      x = false
+      //x = false
 
 
 
 
 
-    } else if (lackSeconds < -60) {
-      document.querySelector('body').innerHTML = '<h1>¡¡¡Feliz cumpleaños, <b>cara de yuca.</b></h1>'
+    //} else if (lackSeconds < -60) {
+      //document.querySelector('body').innerHTML = '<h1>¡¡¡Feliz cumpleaños, <b>cara de yuca.</b></h1>'
 
-    } else {
+    //} else {
 
-      timeContainer.innerHTML = `Faltan <b>${lackSeconds}</b> segundos.`
-      timeCounter.innerHTML =  `La hora actual es: <b>${ currentTimeString }</b>`
-      birthdayCongrat.innerHTML = "Aún tienes la misma edad."
-      document.querySelector('h1').innerHTML = '¿Cuántos segundos faltan para el cumpleaños de Yanna?'
-
-
+      //timeContainer.innerHTML = `Faltan <b>${lackSeconds}</b> segundos.`
+      //timeCounter.innerHTML =  `La hora actual es: <b>${ currentTimeString }</b>`
+      //birthdayCongrat.innerHTML = "Aún tienes la misma edad."
+      //document.querySelector('h1').innerHTML = '¿Cuántos segundos faltan para el cumpleaños de Yanna?'
 
 
-    };
-
-  };
-
-  setInterval(() => obtainTimeString(), 1000)
 
 
-    
+    //};
 
-  
-  console.log('hola')
+  //};
+
+  //setInterval(() => obtainTimeString(), 1000)
+
+  birthdayCongrat.innerHTML = "!!!Feliz cumpleaños, querida prima¡¡¡ Deseo que tu vida esté repleta de felicidad y cosas buenas y que puedas lograr tus metas y deseos. Espero que hayan muchísimos más días como este y que cada uno sea mejor que el anterior. . "
+
+  birthdayCongrat.style.textAlign = 'left'
+  document.querySelector('h1').innerHTML = 'Feliz 23 años, yuca.'
+
+  const yannaImage = document.createElement('img')
+  yannaImage.src = 'yanna_picture.jpeg'
+
+
+  document.querySelector('body').append(yannaImage)
+  yannaImage.style.width = '60vw'
 
 
   
